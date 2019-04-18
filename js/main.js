556d368bcda5003dbd6c6d13a71ee1b4bf900da0
Relt <31004181+rekter@users.noreply.github.com>
2019-04-18 13:37:09 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//alert ('Hello, World!');
+
+var name = 'Hello';
+//alert(name);
+
+var stringValue             = 'Строка'; //Строка
+var numberValue             = 1337; //Число
+var logicValue              =true; //false
+var nonExistant             =null; // ничего
+var notDefined              =undefined; //не задано
+var obj                     ={          //объект
+    key1: 'valueA',
+    key2: 'valueB',
+    key3: 'valueC',
+    funcName: function () {
+
+    }
+};
+
+var apple = {
+    color:      'red',
+    size:       'big',
+}
+
+alert(apple.funcName())
