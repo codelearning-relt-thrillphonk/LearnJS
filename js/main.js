3e61f14a9b6f6903a45ff44514f95eb043f9705b
Relt <31004181+rekter@users.noreply.github.com>
2019-04-18 22:06:31 +0300

    Up-to-date

--- js/main.js
+++ js/main.js
@@ -1,25 +1,115 @@
-//alert ('Hello, World!');
+// //alert ('Hello, World!');
 
-var name = 'Hello';
-//alert(name);
+// var name = 'Hello';
+// //alert(name);
 
-var stringValue             = 'Строка'; //Строка
-var numberValue             = 1337; //Число
-var logicValue              =true; //false
-var nonExistant             =null; // ничего
-var notDefined              =undefined; //не задано
-var obj                     ={          //объект
-    key1: 'valueA',
-    key2: 'valueB',
-    key3: 'valueC',
-    funcName: function () {
+// var stringValue             = 'Строка'; //Строка
+// var numberValue             = 1337; //Число
+// var logicValue              =true; //false
+// var nonExistant             =null; // ничего
+// var notDefined              =undefined; //не задано
+// var obj                     ={          //объект
+//     shaKey1: '8174099687A26621F4E2CDD7CC03B3DACEDB3FB962255B1AAFD033CABE831530',
+//     shaKey2: 'B10253764C8B233FB37542E23401C7B450E5A6F9751F3B5A014F6F67E8BC999D',
+//     shaKey3: 'F576104EEBEAB09651D83ACFFC77C8B8C6EAA4B767AEAB24D7DA80F83F51D865',
+//     funcName: function () {
 
-    }
-};
+//     }
+// };
 
-var apple = {
-    color:      'red',
-    size:       'big',
-}
+// var obj                     ={
+//     miner1: 'Working',
+//     miner2: 'Problem'
+// };
 
-alert(apple.funcName())
+// var apple = {
+//     color:      'red',
+//     size:       'big',
+// };
+
+// //alert(apple.funcName())
+
+// //Взаимодействие с пользователем
+// /*alert('Hello, Guest!');
+// var answer = prompt ('Who ar u?');
+// alert(name + ', ' + answer + '!');
+// alert('Nice name)');
+// */
+// // var answerNew = confirm ('u lived in US?');
+
+// // Условие
+
+// // if (obj.miner1 == 'Working') {
+// //     alert('miner1 Works - Normal')
+// // } else {
+// //     alert('miner1 Works - Problem')
+// // }
+
+// // if (obj.miner2 == 'Working') {
+// //     alert('miner2 Works - Normal')
+// // } else {
+// //     alert('miner2 Works - Problem')
+// // }
+
+// // Циклы (while, doWhile, for)
+
+// // for (var i = 0; i <= 5; i++) {
+// //     alert(i);
+// // }
+
+// // Функции
+
+// // function thVariable (m, n) {
+// //     var inFunc = 21;
+// //     alert ('You age is ' + inFunc)
+// //     alert((m/n) * 100 + '%');
+// // }
+
+// // thVariable (5, 10);
+
+// // // Массив
+
+// // var massive = ['flex', 'cringe', 'chill'];
+
+// // massive(0) ;
+
+// // DOM
+
+
+
+// // События
+
+
+
+
+
+
+// function __(selector) {
+//     return document.body.querySelector('selector');
+// }
+
+// __('.block');
+
+
+// // jQuery
+
+
+
+
+
+
+// //////////////////////////////////////////////////
+
+// var elem = document.body.querySelector('.button');
+
+// var block = {
+//     style: {
+//         backgroundColor: 'blue'
+//     },
+//     size: 'big',
+// };
+
+// elem.onclick = function() {
+//     var eZ = document.body.querySelector('.block');
+//     eZ.style.backgroundColor = 'red'
+// }
